04c6e7852a2216aeebc152c3c23734ca6241ac49
fs203 <fs2031@icloud.com>
2025-05-04 14:59:37 +0900

    added : Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Box,
+  ChakraProvider,
+  defaultSystem,
+  HStack,
+  Link,
+  Separator,
+} from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ChakraProvider value={defaultSystem}>
+      <Box bg="gray.900" minH="100vh" minW="100vw">
+        <Box>
+          <HStack spacing={4} p={4} justify="flex-end">
+            <Link color="white" href="#" _hover={{ color: "cyan.400" }}>
+              HOME
+            </Link>
+            <Link color="white" href="#" _hover={{ color: "cyan.400" }}>
+              ABOUT
+            </Link>
+            <Link color="white" href="#" _hover={{ color: "cyan.400" }}>
+              CONTACT
+            </Link>
+          </HStack>
+          <Separator color="gray.500" />
+        </Box>
+      </Box>
+    </ChakraProvider>
+  );
 }
-
-export default App
+export default App;
